08e81a67a634cf457ff9f75c39d2b0358b8765ec
Priyatham <Priyatham@zuperly.com>
2021-05-11 12:30:22 +0530

    Everything is clean or organised

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom'
+import Starred from './pages/Starred';
+import Home from './pages/Home';
+
 
 function App() {
   return (
     <Switch>
 
-      <Route exact path="/">This is home page</Route>
+      <Route exact path="/"><Home /></Route>
 
-      <Route exact path="/starred"> This is starred </Route>
+      <Route exact path="/starred"><Starred /></Route>
 
       <Route>
         This is a 404 page
       </Route>
-     </Switch>
+    </Switch>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainPageLayout from '../components/MainPageLayout';
+
+const Home = () => {
+    return(
+        <MainPageLayout>
+            Home
+        </MainPageLayout>
+    );
+}
+
+export default Home;

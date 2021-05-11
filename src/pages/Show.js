26f3e66da3511ab867295c8ed14c7b247c5cdd3e
Priyatham <Priyatham@zuperly.com>
2021-05-11 17:24:53 +0530

    styled show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useReducer }  from 'react'
 import { useParams } from 'react-router-dom'
+import Cast from '../components/shows/Cast';
+import Details from '../components/shows/Details';
+import Seasons from '../components/shows/Seasons';
+import { InfoBlock, ShowPageWrapper } from '../components/shows/Show.styled';
+import ShowMainData from '../components/shows/ShowMainData';
 import { apiGet } from '../misc/config';
 
 
@@ -53,7 +58,6 @@ const Show = () => {
 
     },[id])
 
-    console.log(show);
 
     if(isLoading){
         return <div>Data is being loaded</div>
@@ -64,9 +68,40 @@ const Show = () => {
     }
 
     return (
-        <div>
-            this is show page
-        </div>
+        <ShowPageWrapper>
+            <ShowMainData image={show.image} name={show.name} rating ={show.rating} summary={show.summary} tags={show.genres}/>
+
+            <InfoBlock>
+                <h2>
+                    Details
+                </h2>
+                <div>
+                    <Details status={show.status} network={show.network} premierred={show.premierred}/>
+                </div>
+            </InfoBlock>
+
+            <InfoBlock>
+                <h2>
+                    Seasons
+                </h2>
+                <div>
+                    <Seasons seasons = {show._embedded.seasons}/>
+                </div>
+            </InfoBlock>
+
+            <InfoBlock>
+                <h2>
+                    Cast
+                </h2>
+                <div>
+                    <Cast cast={show._embedded.cast}/>
+                </div>
+            </InfoBlock>
+
+        </ShowPageWrapper>
+
+
+
 
     );
 }
